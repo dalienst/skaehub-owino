3e7cec9591e8d0ab1e221fb21d576fb0bae84f2d
Dalienst owino <oduordalienst@gmail.com>
2022-04-14 12:30:01 +0300

    naming convention

--- Day 3/1.py
+++ Day 3/1.py
@@ -1,8 +1,11 @@
 import numpy
 def date_yesterday_today_tomorrow():
-    yesterday = numpy.datetime64('today', 'D') - numpy.timedelta64(1, 'D')
-    today = numpy.datetime64('today', 'D')
-    tomorrow = numpy.datetime64('today', 'D') + numpy.timedelta64(1, 'D')
-    return yesterday, today, tomorrow
+    yesterday = numpy.datetime64('2022-04-14', 'D') - numpy.timedelta64(1, 'D')
+    tod = numpy.datetime64('2022-04-14', 'D')
+    tomorrow = numpy.datetime64('2022-04-14', 'D') + numpy.timedelta64(1, 'D')
+    #return yesterday, today, tomorrow
+    print("Today is date: ", tod)
+    print("Tomorrow is date: ", tomorrow)
+    print("Yesterday was date: ", yesterday)
 
 print(date_yesterday_today_tomorrow())
