cb8e3d4b0a86e3889715bac06d759756ceb06014
Dalienst owino <oduordalienst@gmail.com>
2022-04-14 11:11:30 +0300

    updates on day 3

--- Day 3/2.py
+++ Day 3/2.py
@@ -1,24 +1,7 @@
-import numpy
-# def weekday_number(month, year):
-#     date = 0
-
-# #     if month == 12:
-# #         end = str(year) + '-' + str(month) + '-31'
-# #         begin = str(year) + '-' + str(month) + '-01'
-# #     else:
-# #         if month < 10:
-# #             endM = '-0'+str(month+1)
-# #             beginM = '-0'+str(month)
-# #         else:
-# #             endM = '-'+str(month+1)
-# #             beginM = '-'+str(month)
-
-# #         end = str(year)+endM+'-01'
-# #         begin = str(year)+beginM+'-01'
-# #         return (numpy.datetime64(end) - numpy.datetime64(begin)+date)
-
-# # print(weekday_number(9, 2022))
-
 import numpy as np
-print("Number of weekdays in March 2017:")
-print(np.busday_count('2017-03', '2017-04'))
+def number_of_weekdays():
+    this_month = str(input("Enter the month you want in the format yyyy-mm: "))
+    next_month = str(input("Enter the next month in the format yyyy-mm: "))
+    print("Number of weekdays in this month are: ", np.busday_count(this_month, next_month))
+
+number_of_weekdays()
--- Day 3/3.py
+++ Day 3/3.py
@@ -3,5 +3,9 @@ def size_of_array():
     num = int(input("Number of elements in the array: "))
     arr = np.array(list(map(int, input("Elements of the array: ").strip().split())))
     return arr, arr.size, arr.size*arr.itemsize
+    #return ("Array is: " + arr + "\nSize of memory occupied by array: " + arr.size*arr.itemsize)
+    # return (arr, 
+    #         arr.size, 
+    #         arr.size*arr.itemsize)
 
 print(size_of_array())
