ede1b3c8212064318431fde509ae4e9266f8f0e9
Dalienst owino <oduordalienst@gmail.com>
2022-04-14 11:17:55 +0300

    Updates on question 3 day 3

--- Day 3/3.py
+++ Day 3/3.py
@@ -2,10 +2,19 @@ import numpy as np
 def size_of_array():
     num = int(input("Number of elements in the array: "))
     arr = np.array(list(map(int, input("Elements of the array: ").strip().split())))
-    return arr, arr.size, arr.size*arr.itemsize
+    #return arr, arr.size, arr.size*arr.itemsize
+    print("The array is: ", arr)
+    print("Array size is: ", arr.size)
+    print("Memory occupied by array: ", arr.size*arr.itemsize)
+
+size_of_array()
+
+
+
+
     #return ("Array is: " + arr + "\nSize of memory occupied by array: " + arr.size*arr.itemsize)
     # return (arr, 
     #         arr.size, 
     #         arr.size*arr.itemsize)
 
-print(size_of_array())
+
